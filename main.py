62745f1273d0c79b7bca34a01928e5acad1f5b0c
victorjefferson <vrjefferso33@gmail.com>
2023-02-06 19:34:12 -0800

    added project titles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as p
 
 st.set_page_config(layout="wide")
 
@@ -12,3 +13,16 @@ with col2:
     content = ''' insert about me
     '''
     st.info(content)
+content = "Below you can find some of the apps I've built with Python, feel free to contact me."
+st.write(content)
+
+col3, col4 = st.columns(2)
+
+df = p.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
